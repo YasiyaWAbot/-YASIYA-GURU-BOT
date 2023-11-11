ad7bdf1a25e419f6412412a6a9428e7100be3a32
YasiyaWAbot <150463099+YasiyaWAbot@users.noreply.github.com>
2023-11-11 01:34:51 -0800

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['94715342700', 'me', true],
+  ['94715342700', 'Yasintha', true], 
   [''] 
 ] //Number of owners
 
